0a954ff5ed143781eca1d570f2df6fc7afb27a5b
Akangkha Sarkar <akangkhasarkar@gmail.com>
2024-03-07 03:13:26 +0530

    Made the TeamMember component responsive

--- ui/TeamMember.tsx
+++ ui/TeamMember.tsx
@@ -5,7 +5,7 @@ import React from "react";
 function TeamMember({ name, image, designation,children }: Readonly<{ children: React.ReactNode }> & TeamMemberProps) {
   return (
     <div
-      className="flex gap-5 p-4 pt-4 pb-2  z-100 w-fit mb-10 flex-col md:flex-row items-center justify-center rounded-lg"
+      className="flex gap-5 p-2 pt-4 pb-2  z-100 w-fit mb-10 flex-col md:flex-row items-center md:items-baseline justify-center md:justify-normal rounded-lg"
       style={{ backgroundColor: "#E8F4FD" }}
     >
       <div
